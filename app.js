ace85351836c840f464a423a7cd15126134d6ac5
wyericso <eric@woooa.net>
2018-08-06 18:28:23 +0800

    Initial build.

--- app.js
+++ app.js
@@ -8,23 +8,38 @@ var port = new SerialPort('/dev/ttyACM0', function (err) {
     }
 });
 
+port.on('data', function(data) {
+    console.log('Data: ', data);
+});
+
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/views/index.html');
 });
 
 app.get('/led', function(req, res) {
-    port.write(Buffer.from([17, 0, 2, 2, 1]), function(err) {
+    port.write(Buffer.from([0x11, 0, 2, 2, 1]), function(err) {
         if (err) {
             return console.log('Error on write: ', err.message);
         }
     });
     setTimeout(() => {
-        port.write(Buffer.from([17, 0, 2, 2, 0]), function(err) {
+        port.write(Buffer.from([0x11, 0, 2, 2, 0]), function(err) {
             if (err) {
                 return console.log('Error on write: ', err.message);
             }
         });
     }, 3000);
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/send_encryption_key', function(req, res) {
+    const buf = Buffer.concat([Buffer.from([0x1F, 2, 0]), Buffer.alloc(512)]);
+    console.log(buf);
+    port.write(buf, function(err) {
+        if (err) {
+            return console.log('Error on write: ', err.message);
+        }
+    });
     res.sendFile(__dirname + '/views/index.html');
 });
 
