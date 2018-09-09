e470fa3b4845a68369f833a0466f7ba7adbeb433
wyericso <eric@woooa.net>
2018-09-09 21:29:10 +0800

    Initial build.

--- app.js
+++ app.js
@@ -15,6 +15,15 @@ port.on('data', function(data) {
     if (encryptionEnabled) {
         data = encryption.Decrypt(data);
     }
+    if (receivingTemplate) {
+        template = Buffer.concat([template, data]);
+        if (template.length == 544) {
+            receivingTemplate = false;
+            template = template.slice(4, 540);
+            console.log('Template: ', template);
+            console.log('Template length: ', template.length);
+        }
+    }
     console.log('Data: ', data);
     console.log('Data length: ', data.length);
 });
@@ -22,7 +31,11 @@ port.on('data', function(data) {
 // Load encryption module.
 
 const encryption = require('./build/Release/encryption');
-var encryptionEnabled = false;
+
+// Initialize variables.
+
+var encryptionEnabled = receivingTemplate = false;
+var template;
 
 // Different actions defined below.
 
@@ -42,9 +55,6 @@ app.get('/ledon', function(req, res) {
         if (err) {
             return console.log('Error on write: ', err.message);
         }
-        else {
-            console.log('Send: ', buf);
-        }
     });
 
     res.redirect('/');
@@ -62,9 +72,6 @@ app.get('/ledoff', function(req, res) {
         if (err) {
             return console.log('Error on write: ', err.message);
         }
-        else {
-            console.log('Send: ', buf);
-        }
     });
 
     res.redirect('/');
@@ -72,7 +79,6 @@ app.get('/ledoff', function(req, res) {
 
 app.get('/send_encryption_key', function(req, res) {
     const buf = Buffer.concat([Buffer.from([0x1f, 0x02, 0x00]), Buffer.alloc(512)]);
-    console.log('Send: ', buf);
 
     port.write(buf, function(err) {
         if (err) {
@@ -87,6 +93,9 @@ app.get('/send_encryption_key', function(req, res) {
 
 app.get('/receive_template', function(req, res) {
     if (encryptionEnabled) {
+        receivingTemplate = true;
+        template = Buffer.from([]);
+
         var buf = Buffer.from([0x15, 0x00, 0x00]);
         buf = Buffer.concat([buf, Buffer.alloc(16 - buf.length)]);
         buf = encryption.Encrypt(buf);
@@ -115,9 +124,6 @@ app.get('/reset', function(req, res) {
     port.write(buf, function(err) {
         if (err) {
             return console.log('Error on write: ', err.message);
-        }
-        else {
-            console.log('Send: ', buf);
         }
     });
 
