f8c3f0bc0adc2715dfb23d20a7d004581cd2256e
wyericso <eric@woooa.net>
2018-09-12 00:02:33 +0800

    Initial build.

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ port.on('data', function(data) {
     }
 });
 
-// Different actions defined below.
+// Different actions / APIs defined below.
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/views/index.html');
@@ -82,6 +82,34 @@ app.get('/ledon', function(req, res) {
     res.redirect('/');
 });
 
+app.get('/api/ledon', function(req, res) {
+    var buf = Buffer.from([0x11, 0x00, 0x02, 0x02, 0x01]);
+
+    if (encryptionEnabled) {
+        buf = Buffer.concat([buf, Buffer.alloc(16 - buf.length)]);
+        buf = encryption.Encrypt(buf);
+    }
+
+    callback = function(response) {
+        // Analyze return code.
+        if (response.readInt8(0) === 0) {
+            res.json({'response': 'ok'});
+        }
+        else {
+            res.json({
+                'response': 'error',
+                'errorCode': response.readInt8(3)
+            });
+        }
+    };
+
+    port.write(buf, function(err) {
+        if (err) {
+            return console.log('Error on write: ', err.message);
+        }
+    });
+});
+
 app.get('/ledoff', function(req, res) {
     var buf = Buffer.from([0x11, 0x00, 0x02, 0x02, 0x00]);
 
@@ -109,6 +137,34 @@ app.get('/ledoff', function(req, res) {
     res.redirect('/');
 });
 
+app.get('/api/ledoff', function(req, res) {
+    var buf = Buffer.from([0x11, 0x00, 0x02, 0x02, 0x00]);
+
+    if (encryptionEnabled) {
+        buf = Buffer.concat([buf, Buffer.alloc(16 - buf.length)]);
+        buf = encryption.Encrypt(buf);
+    }
+
+    callback = function(response) {
+        // Analyze return code.
+        if (response.readInt8(0) === 0) {
+            res.json({'response': 'ok'});
+        }
+        else {
+            res.json({
+                'response': 'error',
+                'errorCode': response.readInt8(3)
+            });
+        }
+    };
+
+    port.write(buf, function(err) {
+        if (err) {
+            return console.log('Error on write: ', err.message);
+        }
+    });
+});
+
 app.get('/send_encryption_key', function(req, res) {
     const buf = Buffer.concat([Buffer.from([0x1f, 0x02, 0x00]), Buffer.alloc(512)]);
 
@@ -128,6 +184,32 @@ app.get('/send_encryption_key', function(req, res) {
         }
     });
     res.redirect('/');
+
+    encryption.Ekeygen();
+    encryptionEnabled = true;
+});
+
+app.get('/api/send_encryption_key', function(req, res) {
+    const buf = Buffer.concat([Buffer.from([0x1f, 0x02, 0x00]), Buffer.alloc(512)]);
+
+    callback = function(response) {
+        // Analyze return code.
+        if (response.readInt8(0) === 0) {
+            res.json({'response': 'ok'});
+        }
+        else {
+            res.json({
+                'response': 'error',
+                'errorCode': response.readInt8(3)
+            });
+        }
+    };
+
+    port.write(buf, function(err) {
+        if (err) {
+            return console.log('Error on write: ', err.message);
+        }
+    });
 
     encryption.Ekeygen();
     encryptionEnabled = true;
@@ -171,6 +253,47 @@ app.get('/receive_template', function(req, res) {
     res.redirect('/');
 });
 
+app.get('/api/receive_template', function(req, res) {
+    template = Buffer.from([]);
+
+    var buf = Buffer.from([0x15, 0x00, 0x00]);
+
+    if (encryptionEnabled) {
+        buf = Buffer.concat([buf, Buffer.alloc(16 - buf.length)]);
+        buf = encryption.Encrypt(buf);
+    }
+
+    callback = function(response) {
+        // Analyze return code.
+        if (response.readInt8(0) === 0) {
+            // If checksum is correct.
+            if (response.readInt32BE(540) === checksum(response.slice(0, 540))) {
+                template = response.slice(4, 540);
+                res.json({'response': 'ok'});
+            }
+            else {
+                template = false;
+                res.json({
+                    'response': 'error',
+                    'errorMsg': 'Invalid checksum.'
+                });
+            }
+        }
+        else {
+            res.json({
+                'response': 'error',
+                'errorCode': response.readInt8(3)
+            });
+        }
+    };
+
+    port.write(buf, function(err) {
+        if (err) {
+            return console.log('Error on write: ', err.message);
+        }
+    });
+});
+
 app.get('/send_template', function(req, res) {
     var buf = Buffer.from([0x12, 0x02, 0x1d, templateNumber++]);
 
@@ -210,6 +333,46 @@ app.get('/send_template', function(req, res) {
     res.redirect('/');
 });
 
+app.get('/api/send_template', function(req, res) {
+    var buf = Buffer.from([0x12, 0x02, 0x1d, templateNumber++]);
+
+    if (templateNumber === 100) {
+        templateNumber = 0;
+    }
+
+    if (template) {
+        buf = Buffer.concat([buf, template]);
+    }
+
+    // Adding checksum.  
+    var csBuf = Buffer.alloc(4);
+    csBuf.writeInt32BE(checksum(buf), 0);
+    buf = Buffer.concat([buf, csBuf]);
+
+    if (encryptionEnabled) {
+        buf = encryption.Encrypt(buf);
+    }
+
+    callback = function(response) {
+        // Analyze return code.
+        if (response.readInt8(0) === 0) {
+            res.json({'response': 'ok'});
+        }
+        else {
+            res.json({
+                'response': 'error',
+                'errorCode': response.readInt8(3)
+            });
+        }
+    };
+
+    port.write(buf, function(err) {
+        if (err) {
+            return console.log('Error on write: ', err.message);
+        }
+    });
+});
+
 app.get('/verification_1toN', function(req, res) {
     var buf = Buffer.from([0x13, 0x00, 0x02, 0x08]);
 
@@ -239,6 +402,37 @@ app.get('/verification_1toN', function(req, res) {
     res.redirect('/');
 });
 
+app.get('/api/verification_1toN', function(req, res) {
+    var buf = Buffer.from([0x13, 0x00, 0x02, 0x08]);
+
+    if (encryptionEnabled) {
+        buf = Buffer.concat([buf, Buffer.alloc(16 - buf.length)]);
+        buf = encryption.Encrypt(buf);
+    }
+
+    callback = function(response) {
+        // Analyze return code.
+        if (response.readInt8(0) === 0) {
+            res.json({
+                'response': 'ok',
+                'verifiedTemplateNumber': response.readInt8(3)
+            });
+        }
+        else {
+            res.json({
+                'response': 'error',
+                'errorCode': response.readInt8(3)
+            });
+        }
+    };
+
+    port.write(buf, function(err) {
+        if (err) {
+            return console.log('Error on write: ', err.message);
+        }
+    });
+});
+
 app.get('/reset', function(req, res) {
     var buf = Buffer.from([0x17, 0x00, 0x00]);
 
@@ -265,6 +459,36 @@ app.get('/reset', function(req, res) {
 
     encryptionEnabled = false;
     res.redirect('/');
+});
+
+app.get('/api/reset', function(req, res) {
+    var buf = Buffer.from([0x17, 0x00, 0x00]);
+
+    if (encryptionEnabled) {
+        buf = Buffer.concat([buf, Buffer.alloc(16 - buf.length)]);
+        buf = encryption.Encrypt(buf);
+    }
+
+    callback = function(response) {
+        // Analyze return code.
+        if (response.readInt8(0) === 0) {
+            res.json({'response': 'ok'});
+        }
+        else {
+            res.json({
+                'response': 'error',
+                'errorCode': response.readInt8(3)
+            });
+        }
+    };
+
+    port.write(buf, function(err) {
+        if (err) {
+            return console.log('Error on write: ', err.message);
+        }
+    });
+
+    encryptionEnabled = false;
 });
 
 process.on('SIGINT', function() {
