b5a8cc836df198131bde8119684e15d6d017c572
wyericso <eric@woooa.net>
2018-10-23 14:30:42 +0800

    change port number in test.sh

--- test.sh
+++ test.sh
@@ -6,11 +6,11 @@
 ## - Not including receive_template and verification_1toN as they need human interaction.
 ##
 
-curl -w "\n" -H "Content-Type:application/json" "http://localhost/api/reset"
-curl -w "\n" -H "Content-Type:application/json" "http://localhost/api/ledon"
-curl -w "\n" -H "Content-Type:application/json" "http://localhost/api/ledoff"
-curl -w "\n" -H "Content-Type:application/json" "http://localhost/api/send_encryption_key"
-curl -w "\n" -H "Content-Type:application/json" -d@/home/eric/index_l.json "http://localhost/api/send_template"
-curl -w "\n" -H "Content-Type:application/json" -d@/home/eric/middle_l.json "http://localhost/api/send_template"
-curl -w "\n" -H "Content-Type:application/json" -d@/home/eric/ring_l.json "http://localhost/api/send_template"
-curl -w "\n" -H "Content-Type:application/json" -d@/home/eric/pinky_l.json "http://localhost/api/send_template"
+curl -w "\n" -H "Content-Type:application/json" "http://localhost:8080/api/reset"
+curl -w "\n" -H "Content-Type:application/json" "http://localhost:8080/api/ledon"
+curl -w "\n" -H "Content-Type:application/json" "http://localhost:8080/api/ledoff"
+curl -w "\n" -H "Content-Type:application/json" "http://localhost:8080/api/send_encryption_key"
+curl -w "\n" -H "Content-Type:application/json" -d@/home/eric/index_l.json "http://localhost:8080/api/send_template"
+curl -w "\n" -H "Content-Type:application/json" -d@/home/eric/middle_l.json "http://localhost:8080/api/send_template"
+curl -w "\n" -H "Content-Type:application/json" -d@/home/eric/ring_l.json "http://localhost:8080/api/send_template"
+curl -w "\n" -H "Content-Type:application/json" -d@/home/eric/pinky_l.json "http://localhost:8080/api/send_template"
